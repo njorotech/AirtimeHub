b5cee390f86e4979a56eee1cec03a74f415137c9
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-03-22 20:59:07 +0300

    Removed login route

--- AirtimeApp.py
+++ AirtimeApp.py
@@ -5,16 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success',name = user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name = user))
-    
+   
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
